d72314db1b663c4a71fad8421a7c73c13c0caf9a
Parjal Rai <parjal.rai@dvla.gov.uk>
2025-08-07 21:07:53 +0100

    create training and test set

--- data_analysis.py
+++ data_analysis.py
@@ -47,3 +47,20 @@ le = LabelEncoder()
 y  = le.fit_transform(y)
 print("For dependant variables, 0 corresponds to 'no' and 1 corresponds to '1'")
 print(y, "\n")
+
+
+print("\nFeature Scaling - happens after splitting dataset into training set and test set")
+print("\n Feature scaling gets mean and standard deviation including the ones in the test set")
+
+
+print("\n\n Let's split the dataset into Training set and Test set")
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)    
+print("\nX_train - represents 8 random customers taken from the dataset\n", X_train, "\n")
+print("X_test- the last 2 columns are age and salary\n", X_test, "\n")
+print("y_train - this is the 8 train purchase data\n", y_train, "\n")
+print("y_test\n", y_test, "\n")
+
+
+print("\n\nFeature Scaling - happens after splitting dataset into training set and test set")
